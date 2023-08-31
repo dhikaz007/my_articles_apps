9fcbdacebffc3b8ec4fd265d60e2abe63da2e765
Andhika <moore.dhikaz@gmail.com>
2023-08-31 11:16:03 +0700

    fix switch case in textStyle

--- lib/core/constants/app_text.dart
+++ lib/core/constants/app_text.dart
@@ -10,26 +10,19 @@ enum AppTextStyle {
   title3,
   body1,
   body2,
-  body3,
+  body3;
 }
 
 enum CustomFontWeight {
   normal,
   medium,
-  bold,
-}
+  bold;
 
-extension FontWeightIndicator on CustomFontWeight {
-  FontWeight get valueFontWeight {
-    switch (this) {
-      case CustomFontWeight.normal:
-        return FontWeight.w400;
-      case CustomFontWeight.medium:
-        return FontWeight.w500;
-      case CustomFontWeight.bold:
-        return FontWeight.w700;
-    }
-  }
+  FontWeight get value => switch (this) {
+        normal => FontWeight.w400,
+        medium => FontWeight.w500,
+        bold => FontWeight.w700,
+      };
 }
 
 class AppText extends StatelessWidget {
@@ -64,55 +57,55 @@ class AppText extends StatelessWidget {
         case AppTextStyle.display1:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 36,
           );
         case AppTextStyle.headline1:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 28,
           );
         case AppTextStyle.headline2:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 24,
           );
         case AppTextStyle.title1:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 20,
           );
         case AppTextStyle.title2:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 18,
           );
         case AppTextStyle.title3:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 16,
           );
         case AppTextStyle.body1:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 14,
           );
         case AppTextStyle.body2:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 12,
           );
         case AppTextStyle.body3:
           return TextStyle(
             color: color,
-            fontWeight: fontWeight.valueFontWeight,
+            fontWeight: fontWeight.value,
             fontSize: 10,
           );
       }
