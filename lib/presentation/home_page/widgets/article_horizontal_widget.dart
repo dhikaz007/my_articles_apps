5faf2231ddfa12ded71a4b3afece37d0e2e40a96
Andhika <moore.dhikaz@gmail.com>
2023-09-22 11:17:39 +0700

    fix shadows

--- lib/presentation/home_page/widgets/article_horizontal_widget.dart
+++ lib/presentation/home_page/widgets/article_horizontal_widget.dart
@@ -33,20 +33,20 @@ class _ArticleHorizontalWidgetState extends State<ArticleHorizontalWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 220,
-      height: 180,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.transparent,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            offset: const Offset(0, 4),
-            blurRadius: 2,
-            spreadRadius: 4,
-          ),
-        ],
-      ),
+      // width: 220,
+      // height: 180,
+      // decoration: BoxDecoration(
+      //   borderRadius: BorderRadius.circular(12),
+      //   color: Colors.transparent,
+      //   boxShadow: [
+      //     BoxShadow(
+      //       color: Colors.grey.withOpacity(0.2),
+      //       offset: const Offset(0, 4),
+      //       blurRadius: 2,
+      //       spreadRadius: 4,
+      //     ),
+      //   ],
+      // ),
       child: Container(
         padding: const EdgeInsets.all(8),
         width: 220,
@@ -55,6 +55,18 @@ class _ArticleHorizontalWidgetState extends State<ArticleHorizontalWidget> {
           color: AppColors.primaryWhite,
           borderRadius: BorderRadius.circular(12),
           border: Border.all(color: AppColors.primaryBlack),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(1),
+              offset: const Offset(0, 5),
+              blurRadius: 5,
+            ),
+            BoxShadow(
+              color: AppColors.primaryWhite,
+              offset: const Offset(-10, 0),
+              //blurRadius: 5,
+            ),
+          ],
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
--- lib/presentation/home_page/widgets/article_vertical_widget.dart
+++ lib/presentation/home_page/widgets/article_vertical_widget.dart
@@ -39,150 +39,141 @@ class _ArticleVerticalWidgetState extends State<ArticleVerticalWidget> {
     final dateFormat = DateFormat('d MMMM y,').add_Hms().format(date);
 
     return Container(
-      width: double.maxFinite,
+      padding: const EdgeInsets.all(12),
+      width: double.infinity,
       height: 220,
       decoration: BoxDecoration(
+        color: AppColors.blueFlax,
         borderRadius: BorderRadius.circular(12),
-        color: Colors.transparent,
         boxShadow: [
-          BoxShadow(
-            color: AppColors.jadeJewel.withOpacity(0.1),
-            offset: const Offset(0, 4),
-            blurRadius: 2,
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        width: double.infinity,
-        height: 220,
-        decoration: BoxDecoration(
-          color: AppColors.blueFlax,
-          borderRadius: BorderRadius.circular(12),
+        BoxShadow(
+          color: AppColors.jadeJewel.withOpacity(0.6),
+          offset: const Offset(0, 5),
+          blurRadius: 5,
         ),
-        child: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Wrap(
-                spacing: 8,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: CachedNetworkImage(
-                      imageUrl: widget.image,
-                      width: 100,
-                      height: 100,
-                      placeholder: (context, url) {
-                        if (url.isNotEmpty) {
-                          return const RectangleShimmer();
-                        }
-                        return AppText(
-                          context: context,
-                          text: 'NO IMAGE',
-                          style: AppTextStyle.title3,
-                          fontWeight: CustomFontWeight.bold,
-                          color: AppColors.blueFlax,
-                        );
-                      },
-                    ),
+      ],
+      ),
+      child: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              spacing: 8,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: CachedNetworkImage(
+                    imageUrl: widget.image,
+                    width: 100,
+                    height: 100,
+                    placeholder: (context, url) {
+                      if (url.isNotEmpty) {
+                        return const RectangleShimmer();
+                      }
+                      return AppText(
+                        context: context,
+                        text: 'NO IMAGE',
+                        style: AppTextStyle.title3,
+                        fontWeight: CustomFontWeight.bold,
+                        color: AppColors.blueFlax,
+                      );
+                    },
                   ),
-                  SizedBox(
-                    width: 220,
-                    height: 50,
-                    child: FutureBuilder(
-                      future:
-                          GoogleTranslator().translate(widget.title, to: code),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const RectangleShimmer();
-                        } else {
-                          if (snapshot.hasData) {
-                            return AppText(
-                              context: context,
-                              text: snapshot.data?.text ?? '-',
-                              style: AppTextStyle.title3,
-                              fontWeight: CustomFontWeight.medium,
-                              color: AppColors.primaryBlack,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            );
-                          }
+                ),
+                SizedBox(
+                  width: 220,
+                  height: 50,
+                  child: FutureBuilder(
+                    future:
+                        GoogleTranslator().translate(widget.title, to: code),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const RectangleShimmer();
+                      } else {
+                        if (snapshot.hasData) {
                           return AppText(
                             context: context,
-                            text: 'Data empty',
+                            text: snapshot.data?.text ?? '-',
                             style: AppTextStyle.title3,
                             fontWeight: CustomFontWeight.medium,
                             color: AppColors.primaryBlack,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
                           );
                         }
-                      },
-                    ),
+                        return AppText(
+                          context: context,
+                          text: 'Data empty',
+                          style: AppTextStyle.title3,
+                          fontWeight: CustomFontWeight.medium,
+                          color: AppColors.primaryBlack,
+                        );
+                      }
+                    },
                   ),
-                ],
-              ),
-              const SizedBox(height: 4),
-              FutureBuilder(
-                future: GoogleTranslator().translate(widget.content, to: code),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const RectangleShimmer(
-                      w: double.maxFinite,
-                      h: 50,
-                    );
-                  } else {
-                    if (snapshot.hasData) {
-                      return AppText(
-                        context: context,
-                        text: snapshot.data?.text ?? '-',
-                        style: AppTextStyle.title3,
-                        fontWeight: CustomFontWeight.normal,
-                        color: AppColors.primaryBlack,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      );
-                    }
+                ),
+              ],
+            ),
+            const SizedBox(height: 4),
+            FutureBuilder(
+              future: GoogleTranslator().translate(widget.content, to: code),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const RectangleShimmer(
+                    w: double.maxFinite,
+                    h: 50,
+                  );
+                } else {
+                  if (snapshot.hasData) {
                     return AppText(
                       context: context,
-                      text: 'Data empty',
+                      text: snapshot.data?.text ?? '-',
                       style: AppTextStyle.title3,
-                      fontWeight: CustomFontWeight.medium,
+                      fontWeight: CustomFontWeight.normal,
                       color: AppColors.primaryBlack,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     );
                   }
-                },
-              ),
-              const SizedBox(height: 20),
-              FutureBuilder(
-                future: Future.delayed(const Duration(milliseconds: 1500)),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Align(
-                      alignment: Alignment.bottomRight,
-                      child: RectangleShimmer(),
-                    );
-                  } else {
-                    return Align(
-                      alignment: Alignment.bottomRight,
-                      child: AppText(
-                        context: context,
-                        text: dateFormat,
-                        style: AppTextStyle.body1,
-                        fontWeight: CustomFontWeight.normal,
-                        color: AppColors.primaryBlack,
-                        textAlign: TextAlign.left,
-                        maxLines: 3,
-                      ),
-                    );
-                  }
-                },
-              ),
-            ],
-          ),
+                  return AppText(
+                    context: context,
+                    text: 'Data empty',
+                    style: AppTextStyle.title3,
+                    fontWeight: CustomFontWeight.medium,
+                    color: AppColors.primaryBlack,
+                  );
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            FutureBuilder(
+              future: Future.delayed(const Duration(milliseconds: 1500)),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Align(
+                    alignment: Alignment.bottomRight,
+                    child: RectangleShimmer(),
+                  );
+                } else {
+                  return Align(
+                    alignment: Alignment.bottomRight,
+                    child: AppText(
+                      context: context,
+                      text: dateFormat,
+                      style: AppTextStyle.body1,
+                      fontWeight: CustomFontWeight.normal,
+                      color: AppColors.primaryBlack,
+                      textAlign: TextAlign.left,
+                      maxLines: 3,
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
